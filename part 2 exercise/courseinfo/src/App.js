cf602a09f8a8f1acc94178ee2b927c9234e79b6c
PravallikaVudata <74001336+PravallikaVudata@users.noreply.github.com>
2021-06-05 11:05:21 +0530

    Add files via upload

--- part 2 exercise/courseinfo/src/App.js
+++ part 2 exercise/courseinfo/src/App.js
@@ -1,29 +1,60 @@
-import React from 'react'
-
+import React from 'react';
+import Note from './components/Note'
 const App = () => {
-  const s = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
+  const courses = [
+    {
+      name:'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name:'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name:'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name:'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name:'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name:'Node.js',
+      id: 2,
+      parts: [
+        {
+          name:'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name:'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return(
     <div>
-      <h1>{s}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+      <h1>WEB DEVELOPMENT CURRICULUM</h1>
+      <Subject  courses2={courses}/>
+      </div>
   )
 }
-
+const Subject = ({ courses2 }) => (
+  <div>
+    <Note course={courses2} />
+  </div>
+)
 export default App
